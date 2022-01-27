413e5064a779f87660634e92f7b60577b9e54f33
Son Nguyen <nvs2394@gmail.com>
2022-01-27 17:55:54 +0800

    fix: wrong aws config

--- infra/output.tf
+++ infra/output.tf
@@ -2,10 +2,6 @@ output "elb" {
   value = aws_elb.api-customer-elb.dns_name
 }
 
-output "jenkins" {
-  value = aws_instance.jenkins-instance.public_ip
-}
-
 output "api-customer-repository-URL" {
   value = aws_ecr_repository.api-customer.repository_url
 }
