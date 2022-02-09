a3400d95a29a803c67877582051ff11b91417235
Son Nguyen <nvs2394@gmail.com>
2022-02-10 01:04:19 +0800

    fix: use http only

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -205,27 +205,32 @@ resource "aws_lb_listener" "listener" {
   port              = "80"
   protocol          = "HTTP"
 
-  default_action {
-    type = "redirect"
+  # default_action {
+  #   type = "redirect"
 
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_listener" "listener-https" {
-  load_balancer_arn = aws_alb.application_load_balancer.id
-  port              = "443"
-  protocol          = "HTTPS"
-
-  ssl_policy      = "ELBSecurityPolicy-2016-08"
-  certificate_arn = "<certificate-arn>"
+  #   redirect {
+  #     port        = "443"
+  #     protocol    = "HTTPS"
+  #     status_code = "HTTP_301"
+  #   }
+  # }
 
   default_action {
-    target_group_arn = aws_lb_target_group.target_group.id
     type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.id
   }
 }
+
+# resource "aws_lb_listener" "listener-https" {
+#   load_balancer_arn = aws_alb.application_load_balancer.id
+#   port              = "443"
+#   protocol          = "HTTPS"
+
+#   ssl_policy      = "ELBSecurityPolicy-2016-08"
+#   certificate_arn = "<certificate-arn>"
+
+#   default_action {
+#     target_group_arn = aws_lb_target_group.target_group.id
+#     type             = "forward"
+#   }
+# }
