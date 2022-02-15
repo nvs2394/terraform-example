f0826432753eae5b0b1209b67440e1a9f1ac6872
Son Nguyen <nvs2394@gmail.com>
2022-02-15 14:04:46 +0800

    infra: separate alb definition in ecs

---
+++ infra/alb.tf
@@ -0,0 +1,111 @@
+# alb.tf | Application Load Balancer
+
+resource "aws_alb" "application_load_balancer" {
+  name               = "${var.app_name}-${var.app_environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public.*.id
+  security_groups    = [aws_security_group.load_balancer_security_group.id]
+
+  tags = {
+    Name        = "${var.app_name}-alb"
+    Environment = var.app_environment
+  }
+}
+# alb_securitygroup.tf | Application Load Balancer Security Group
+
+resource "aws_security_group" "load_balancer_security_group" {
+  vpc_id = aws_vpc.aws-vpc.id
+
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name        = "${var.app_name}-sg"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "${var.app_name}-${var.app_environment}-${substr(uuid(), 0, 3)}-tg"
+  port        = 8080
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.aws-vpc.id
+
+  health_check {
+    healthy_threshold   = "5"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "5"
+    path                = "/v1/status"
+    unhealthy_threshold = "2"
+    port = "8080"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [name]
+  }
+
+  tags = {
+    Name        = "${var.app_name}-lb-tg"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  # default_action {
+  #   type = "redirect"
+
+  #   redirect {
+  #     port        = "443"
+  #     protocol    = "HTTPS"
+  #     status_code = "HTTP_301"
+  #   }
+  # }
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.id
+  }
+}
+
+# resource "aws_lb_listener" "listener-https" {
+#   load_balancer_arn = aws_alb.application_load_balancer.id
+#   port              = "443"
+#   protocol          = "HTTPS"
+
+#   ssl_policy      = "ELBSecurityPolicy-2016-08"
+#   certificate_arn = "<certificate-arn>"
+
+#   default_action {
+#     target_group_arn = aws_lb_target_group.target_group.id
+#     type             = "forward"
+#   }
+# }
