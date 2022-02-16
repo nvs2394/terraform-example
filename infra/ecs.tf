f90e0c989d0d1bd30f1fea4e201560eeb0fd061f
Son Nguyen <nvs2394@gmail.com>
2022-02-16 11:31:00 +0800

    infra: revert changed

--- infra/ecs.tf
+++ infra/ecs.tf
@@ -115,24 +115,9 @@ resource "aws_security_group" "service_security_group" {
     security_groups = [aws_security_group.load_balancer_security_group.id]
   }
 
-    ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "-1"
-    security_groups = [aws_security_group.load_balancer_security_group.id]
-  }
-
   egress {
     from_port        = 0
     to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 443
-    to_port          = 443
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
